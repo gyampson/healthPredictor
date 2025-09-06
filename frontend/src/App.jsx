3e9ebc2c74bc503e332801959089b1d61ab37220
gyampson <sgyampoh005@st.ug.edu.gh>
2025-09-05 17:30:45 -0700

    Third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://healthpredictorbackend.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
